55417eca932d0b2e13cc27dafd2977be9dcfbe6a
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-09-27 17:53:04 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,12 @@
-require 'pry'
 
 def my_collect(array)
   i=0
   collection = []
-  binding.pry
   while i < array.length
     collection << yield(array[i])
     i+=0
   end
+  
   collection
 end
 
